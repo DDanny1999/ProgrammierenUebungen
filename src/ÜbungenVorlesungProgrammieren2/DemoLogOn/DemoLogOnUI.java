a10a643bdf6380411f9951baff86feb0bfb12ff6
Daniel <danny.melchior28041999@gmail.com>
2022-04-04 16:47:29 +0200

    New

--- src/ÜbungenVorlesungProgrammieren2/DemoLogOn/DemoLogOnUI.java
+++ src/ÜbungenVorlesungProgrammieren2/DemoLogOn/DemoLogOnUI.java
@@ -10,6 +10,48 @@ public class DemoLogOnUI extends JFrame {
     private DemoLogOnUI(){
         super("Logon");
 
+        JPanel userPanel = new JPanel();
+        userPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        userPanel.add(new JLabel("User:            "));
+        userPanel.add(new JTextField(5));
+
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        passwordPanel.add(new JLabel("Password:  "));
+        passwordPanel.add(new JPasswordField(6));
+
+        JPanel artPanel = new JPanel();
+        artPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        artPanel.add(new JLabel("Art:                "));
+
+        String[] comboBoxListe = new String[2];
+        comboBoxListe[0] = "FTP";
+        comboBoxListe[1] = "P2P";
+
+        JComboBox art = new JComboBox(comboBoxListe);
+
+        artPanel.add(art);
+
+        JPanel hostPanel = new JPanel();
+        hostPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        hostPanel.add(new JLabel("Host:            "));
+        hostPanel.add(new JTextField(6));
+
+        JPanel portPanel = new JPanel();
+        portPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        portPanel.add(new JLabel("Port:             "));
+        portPanel.add(new JTextField(1));
+
+        JPanel quellePanel = new JPanel();
+        quellePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        quellePanel.add(new JLabel("Quelle:           "));
+        quellePanel.add(new JTextField(8));
+
+        JPanel zielPanel = new JPanel();
+        zielPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        zielPanel.add(new JLabel("Ziel:                "));
+        zielPanel.add(new JTextField(8));
+
         JPanel rootWindow = new JPanel();
         rootWindow.setLayout(new BorderLayout());
 
@@ -20,26 +62,13 @@ public class DemoLogOnUI extends JFrame {
         Border blauesPanelLinksBorder = BorderFactory.createTitledBorder("Verbindung");
 
         JPanel blauesPanelLinks = new JPanel();
-        blauesPanelLinks.setLayout(new GridLayout(5,2));
+        blauesPanelLinks.setLayout(new GridLayout(5,1));
 
-        blauesPanelLinks.add(new JLabel("User: "));
-        blauesPanelLinks.add(new JTextField());
-        blauesPanelLinks.add(new JLabel("Password: "));
-        blauesPanelLinks.add(new JPasswordField());
-        blauesPanelLinks.add(new JLabel("Art: "));
-
-        String[] comboBoxListe = new String[2];
-        comboBoxListe[0] = "FTP";
-        comboBoxListe[1] = "P2P";
-
-        JComboBox art = new JComboBox(comboBoxListe);
-
-        blauesPanelLinks.add(art);
-
-        blauesPanelLinks.add(new JLabel("Host: "));
-        blauesPanelLinks.add(new JTextField());
-        blauesPanelLinks.add(new JLabel("Port: "));
-        blauesPanelLinks.add(new JTextField());
+        blauesPanelLinks.add(userPanel);
+        blauesPanelLinks.add(passwordPanel);
+        blauesPanelLinks.add(artPanel);
+        blauesPanelLinks.add(hostPanel);
+        blauesPanelLinks.add(portPanel);
 
         blauesPanelLinks.setBorder(blauesPanelLinksBorder);
 
@@ -54,12 +83,10 @@ public class DemoLogOnUI extends JFrame {
         Border blauesPanelRechtsBorder = BorderFactory.createTitledBorder("Dateien");
 
         JPanel blauesPanelRechts = new JPanel();
-        blauesPanelRechts.setLayout(new GridLayout(2,2));
+        blauesPanelRechts.setLayout(new GridLayout(2,1));
 
-        blauesPanelRechts.add(new JLabel("Quelle: "));
-        blauesPanelRechts.add(new JTextField());
-        blauesPanelRechts.add(new JLabel("Ziel: "));
-        blauesPanelRechts.add(new JTextField());
+        blauesPanelRechts.add(quellePanel);
+        blauesPanelRechts.add(zielPanel);
 
         blauesPanelRechts.setBorder(blauesPanelRechtsBorder);
 
