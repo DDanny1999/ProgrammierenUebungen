be68f7739e4fc5dbfffe94025ed85c953a1bf0bb
Daniel <danny.melchior28041999@gmail.com>
2022-04-05 15:44:24 +0200

    New

--- src/ÜbungenVorlesungProgrammieren2/DemoLogOn/DemoLogOnUI.java
+++ src/ÜbungenVorlesungProgrammieren2/DemoLogOn/DemoLogOnUI.java
@@ -3,24 +3,33 @@ package ÜbungenVorlesungProgrammieren2.DemoLogOn;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.text.MaskFormatter;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class DemoLogOnUI extends JFrame {
 
-    private DemoLogOnUI(){
+    private DemoLogOnUI() throws ParseException {
         super("Logon");
+
+        // MaskFormater's
+
+        MaskFormatter intOnly = new MaskFormatter("##");
+        MaskFormatter stringOnlyBIG = new MaskFormatter("UUUUUUU");
+        MaskFormatter stringOnly = new MaskFormatter("???????");
 
         // JPANELS für einzelne Felder
 
         JPanel userPanel = new JPanel();
         userPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         userPanel.add(new JLabel("User:            "));
-        userPanel.add(new JTextField(5));
+        userPanel.add(new JTextField(4));
 
         JPanel passwordPanel = new JPanel();
         passwordPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         passwordPanel.add(new JLabel("Password:  "));
-        passwordPanel.add(new JPasswordField(6));
+        passwordPanel.add(new JPasswordField(5));
 
         JPanel artPanel = new JPanel();
         artPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -37,12 +46,32 @@ public class DemoLogOnUI extends JFrame {
         JPanel hostPanel = new JPanel();
         hostPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         hostPanel.add(new JLabel("Host:            "));
-        hostPanel.add(new JTextField(6));
+        JFormattedTextField hostField = new JFormattedTextField(stringOnlyBIG);
+        hostField.setColumns(4);
+
+        hostPanel.add(hostField);
 
         JPanel portPanel = new JPanel();
         portPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         portPanel.add(new JLabel("Port:             "));
-        portPanel.add(new JTextField(1));
+
+        // PortFeld
+
+        JFormattedTextField portField = new JFormattedTextField(intOnly);
+        portField.setColumns(2);
+
+        portPanel.add(portField);
+
+//        NumberFormat intFormat = NumberFormat.getIntegerInstance();
+//
+//        NumberFormatter intFormatter = new NumberFormatter(intFormat);
+//        intFormatter.setValueClass(Integer.class); //optional, ensures you will always get a int value
+//        intFormatter.setAllowsInvalid(false); //this is the key!!
+//
+//        JFormattedTextField portField = new JFormattedTextField(intFormatter);
+//        portField.setColumns(4);
+//
+//        portPanel.add(portField);
 
         JPanel quellePanel = new JPanel();
         quellePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -132,7 +161,7 @@ public class DemoLogOnUI extends JFrame {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException{
         new DemoLogOnUI();
     }
 }
